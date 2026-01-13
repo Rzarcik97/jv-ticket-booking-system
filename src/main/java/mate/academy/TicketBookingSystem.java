cd5b3108faf7c4372b275430ffdbd02a3cfb87b5
Karol Gajda <karolgaj97@interia.pl>
2026-01-13 13:38:14 +0100

    Creating Thread with semafhore

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,28 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private final Semaphore semaphore = new Semaphore(1);
+    private int seats;
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.seats = totalSeats;
     }
 
     public BookingResult attemptBooking(String user) {
+        try {
+            semaphore.acquire();
+            if (seats <= 0) {
+                semaphore.release();
+                return new BookingResult(user, false, "No seats available.");
+            }
+            seats--;
+            semaphore.release();
+            return new BookingResult(user, true, "Booking successful.");
+        } catch (InterruptedException e) {
+            System.out.println("Thread interrupted.");
+        }
         return null;
     }
 }
